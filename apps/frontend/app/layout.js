b7237d4b29865cf6fd281d6d51545d85f3b42a45
golden-gab <82877801+golden-gab@users.noreply.github.com>
2026-04-06 09:00:45 +0100

    add google Tag

--- apps/frontend/app/layout.js
+++ apps/frontend/app/layout.js
@@ -15,14 +15,13 @@ const fontTitle = Montserrat({
 const fontText = Roboto({
     variable: "--font2",
     subsets: ["latin"],
-    display: "swap", 
+    display: "swap",
 });
 
 export const metadata = {
     title: "Mealop",
     description:
         "Une solution simple et efficace pour digitaliser vos menus, gérer vos commandes via WhatsApp et suivre vos commandes en toute sérénité.",
-
 };
 
 export default function RootLayout({ children }) {
@@ -35,7 +34,15 @@ export default function RootLayout({ children }) {
                     integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
                     crossOrigin=""
                 /> */}
-                
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-6QFGRW5ML0"
+                ></script>
+                <script>
+                    window.dataLayer = window.dataLayer || []; function gtag()
+                    {dataLayer.push(arguments)}
+                    gtag('js', new Date()); gtag('config', 'G-6QFGRW5ML0');
+                </script>
             </head>
             <body className={`${fontTitle.variable} ${fontText.variable}`}>
                 <IconsLoader />
