4c16a0448c62a146fd7d94321b431ce32ec936fe
golden-gab <82877801+golden-gab@users.noreply.github.com>
2026-04-09 11:42:03 +0100

    Enhance restaurant specialities layout with flex-wrap and adjust font size for better responsiveness

--- apps/frontend/components/guest/restaurant/restoInfo/restoInfo.jsx
+++ apps/frontend/components/guest/restaurant/restoInfo/restoInfo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./restoInfo.css";
+import "./restoInfo.css"; 
 import Image from "next/image";
 import Link from "next/link";
 import { seeMore } from "@/helpers/function";
